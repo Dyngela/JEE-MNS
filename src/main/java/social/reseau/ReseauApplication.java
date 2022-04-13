3005d6c9517acfd68a293dc59fc851504133c588
Dyngela <erwan.nutz@free.fr>
2022-04-13 15:47:57 +0200

    initial commit

---
+++ src/main/java/social/reseau/ReseauApplication.java
@@ -0,0 +1,13 @@
+package social.reseau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReseauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReseauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/social/reseau/ReseauApplicationTests.java
@@ -0,0 +1,13 @@
+package social.reseau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReseauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
