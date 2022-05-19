da83473567a4e2b9e76ff7fe94461e67737367f7
NicolasGretten <ngretten@gmail.com>
2022-05-19 15:03:40 +0200

    forum done

--- src/main/java/social/reseau/controller/ForumController.java
+++ src/main/java/social/reseau/controller/ForumController.java
@@ -1,20 +1,19 @@
 package social.reseau.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import social.reseau.model.Comment;
 import social.reseau.model.Post;
-import social.reseau.model.Users;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/forum")
@@ -30,12 +29,7 @@ public class ForumController {
         RestTemplate restTemplate = new RestTemplate();
         String uri = "http://127.0.0.1:8081/api/posts";
         post.setDate(Timestamp.valueOf(LocalDateTime.now()));
-        post.setIdUser(1L);
-//        post.setUser(new Users(5L, "test", "test", null, null));
-//        System.out.println(post.getDate());
-//        System.out.println(post.getContent());
-//        System.out.println(post.getIdUser());
-        Object result = restTemplate.postForEntity(uri, post, Post.class);
+        restTemplate.postForEntity(uri, post, Post.class);
         return init();
     }
 
@@ -43,11 +37,18 @@ public class ForumController {
     public ModelAndView saveComment(Comment comment) {
         RestTemplate restTemplate = new RestTemplate();
         String uri = "http://127.0.0.1:8081/api/comments";
-        System.out.println(comment.getContent());
-        System.out.println(comment.getIdUser());
-        System.out.println(comment.getIdMessage());
-        System.out.println(comment);
-//        Object result = restTemplate.postForObject(uri, comment, Comment.class);
+        comment.setDate(Timestamp.valueOf(LocalDateTime.now()));
+        Object result = restTemplate.postForEntity(uri, comment, Comment.class);
+        return init();
+    }
+
+    @PostMapping("/deletePost")
+    public ModelAndView deletePost(Post post) {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://127.0.0.1:8081/api/posts/{id}";
+        Map< String, String > params = new HashMap< String, String >();
+        params.put("id", post.getIdMessage().toString());
+        restTemplate.delete(uri, params);
         return init();
     }
 
@@ -56,9 +57,11 @@ public class ForumController {
         RestTemplate restTemplate = new RestTemplate();
         Object result = restTemplate.getForObject(uri, Object.class);
         ModelAndView mv = new ModelAndView("forum");
+        mv.addObject("iduser", 1);
         mv.addObject("posts", result);
         mv.addObject("newPost", new Post());
         mv.addObject("newComment", new Comment());
+        mv.addObject("deletePost", new Post());
         return mv;
     }
 }
